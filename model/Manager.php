0a86c71118f4c4f8de5cd2c7c4743c896ecf3f6c
Guillaumedewolf <33451633+Guillaumedewolf@users.noreply.github.com>
2018-03-08 16:19:22 +0100

    Update Manager.php

--- model/Manager.php
+++ model/Manager.php
@@ -3,7 +3,11 @@ namespace Becode\Blog\Model;
 
 class Manager{
     protected function dbConnect(){
-        $db = new \PDO('pgsql:host=ec2-79-125-12-27.eu-west-1.compute.amazonaws.com;port=5432;dbname=dft6kco7jan4ck;user=uisvlzlfdfoohj;password=85c4888f62f272a9d172d4fb89c3b30d7f82fc5a425058eabe41c908363fa60f');
+        $host_bdd='ec2-79-125-12-27.eu-west-1.compute.amazonaws.com';
+        $name_bdd='dft6kco7jan4ck';
+        $user_bdd='uisvlzlfdfoohj';
+        $pass_bdd='85c4888f62f272a9d172d4fb89c3b30d7f82fc5a425058eabe41c908363fa60f';
+        $db = new \PDO("pgsql:host=".$host_bdd.";dbname=".$name_bdd."", "".$user_bdd."", "".$pass_bdd."");
         
         return $db;
     }
