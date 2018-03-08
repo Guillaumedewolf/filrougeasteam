fc1778af2ce65cbc7620ea8bb586c8173481bfc0
Guillaumedewolf <33451633+Guillaumedewolf@users.noreply.github.com>
2018-03-08 11:59:45 +0100

    Update Manager.php

--- model/Manager.php
+++ model/Manager.php
@@ -3,7 +3,7 @@ namespace Becode\Blog\Model;
 
 class Manager{
     protected function dbConnect(){
-        $db = new \PDO('mysql:host=localhost;dbname=filrougeasteam;charset=utf8', 'root', 'root');
+        $db = new \PDO('mysql:host=ec2-79-125-12-27.eu-west-1.compute.amazonaws.com;dbname=dft6kco7jan4ck;charset=utf8', 'uisvlzlfdfoohj', '85c4888f62f272a9d172d4fb89c3b30d7f82fc5a425058eabe41c908363fa60f');
         
         return $db;
     }
